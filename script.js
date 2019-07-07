95f3f3e596536a51123cd07a857b13bddb01290a
Васин Антон <a.vasin@mediasoft.team>
2019-07-07 23:15:23 +0300

    Task 2

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-alert("Hello Mediasoft!");
+let a, b, c;
+
+function sum() {
+	a = parseInt(prompt("Введите первое число", ""));
+	b = parseInt(prompt("Введите второе число", ""));
+	c = a + b;
+	alert("Результат сложения: " + c);
+}
+
+function sub() {
+	a = parseInt(prompt("Введите первое число", ""));
+	b = parseInt(prompt("Введите второе число", ""));
+	c = a - b;
+	alert("Результат вычитания: " + c);
+}
+
+function mul() {
+	a = parseInt(prompt("Введите первое число", ""));
+	b = parseInt(prompt("Введите второе число", ""));
+	c = a * b;
+	alert("Результат умножения: " + c);
+}
+
+function div() {
+	a = parseInt(prompt("Введите первое число", ""));
+	b = parseInt(prompt("Введите второе число", ""));
+	c = Math.ceil((a / b) * 100) / 100;
+	alert("Результат деления: " + c);
+}
